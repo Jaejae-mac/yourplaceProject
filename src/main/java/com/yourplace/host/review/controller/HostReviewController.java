c6f7a85fb44848e44681ea3ebb21428a300d9952
Baekrihyang <88878737+Baekrihyang@users.noreply.github.com>
2021-09-20 03:02:34 +0900

    Update HostReviewController.java

--- src/main/java/com/yourplace/host/review/controller/HostReviewController.java
+++ src/main/java/com/yourplace/host/review/controller/HostReviewController.java
@@ -3,11 +3,8 @@ package com.yourplace.host.review.controller;
 import java.util.List;
 
 import javax.inject.Inject;
-<<<<<<< HEAD
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
-=======
->>>>>>> 048c1b2f8ce36b365d8039112f0f3e58c6ee13f1
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -22,7 +19,6 @@ public class HostReviewController {
 	HostReviewService service;
 	
 	@RequestMapping(value="/hostReviews.hdo")
-<<<<<<< HEAD
 		public ModelAndView Review(HttpServletRequest request) throws Exception{
 		List<HostReviewVO> list = service.getReviewList();
 		ModelAndView mav = new ModelAndView();
@@ -31,13 +27,6 @@ public class HostReviewController {
 		mav.setViewName("hostReviews");
 		mav.addObject("list", list);
 		mav.addObject("userId", id);
-=======
-		public ModelAndView Review() throws Exception{
-		List<HostReviewVO> list = service.getReviewList();
-		ModelAndView mav = new ModelAndView();
-		mav.setViewName("hostReviews");
-		mav.addObject("list", list);
->>>>>>> 048c1b2f8ce36b365d8039112f0f3e58c6ee13f1
 //		System.out.println(mav.toString());
 		return mav;
 	}
